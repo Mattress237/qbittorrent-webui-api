0d214cec021af6ceb5157ca3830a55b1dddd4f87
mattress <mattress@mattress.no>
2026-06-10 07:46:02 +0200

    Fix: Wrong paths and missing files for tests Some tests for directory and torrent creation had incorrect or missing paths, and the tests specifically on the GitHub action failed to find and use them. added a `.env.example` file for testing

--- tests/mod.rs
+++ tests/mod.rs
@@ -5,7 +5,7 @@ use qbit::{
     parameters::AddTorrentBuilder,
 };
 use rand::{Rng, distr::Alphabetic};
-use std::{env, fs, path};
+use std::{env, fs};
 
 pub mod application;
 pub mod authentication;
@@ -14,6 +14,9 @@ pub mod torrents;
 
 pub const DEBIAN_HASH: &str = "6f4370df4304609a8793ce2b59178dcc8febf5e2";
 pub const DEBIAN_TRACKER: &str = "magnet:?xt=urn:btih:6f4370df4304609a8793ce2b59178dcc8febf5e2&dn=debian-12.11.0-amd64-netinst.iso&xl=702545920&tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce&ws=https://cdimage.debian.org/cdimage/archive/12.11.0/amd64/iso-cd/debian-12.11.0-amd64-netinst.iso&ws=https://cdimage.debian.org/cdimage/release/12.11.0/amd64/iso-cd/debian-12.11.0-amd64-netinst.iso";
+
+// Relative path to the dummy file within the test data directory.
+const DUMMY_FILE: &str = "dummy/dummy.txt";
 
 pub fn get_server_details() -> String {
     dotenv().ok();
@@ -58,6 +61,10 @@ pub async fn add_debian_torrent(client: &Api) {
         .build()
         .expect("Failed to build AddTorrent");
 
+    if client.torrent(DEBIAN_HASH).await.is_ok() {
+        return;
+    }
+
     client
         .add_torrent(param)
         .await
@@ -82,56 +89,82 @@ pub async fn get_debian_torrent(client: &Api) -> Option<Torrent> {
         .map(|t| t.to_owned())
 }
 
-pub fn create_random_name() -> Option<String> {
-    Some(
-        rand::rng()
-            .sample_iter(&Alphabetic)
-            .take(7)
-            .map(char::from)
-            .collect::<String>(),
-    )
+pub fn create_random_name(prefix: &str) -> String {
+    let name = rand::rng()
+        .sample_iter(&Alphabetic)
+        .take(7)
+        .map(char::from)
+        .collect::<String>();
+    format!("{prefix}{name}")
 }
 
-pub fn create_test_data(random_name: Option<String>) -> String {
+// NOTE: The random name is ignored and a dummy file is always created.
+pub fn create_test_data() -> String {
     dotenv().ok();
+    if env::var("temp_dir").is_err() {
+        panic!("'temp_dir' not set. Required for test data creation.");
+    }
+    if env::var("server_temp_dir").is_err() {
+        panic!("'server_temp_dir' not set. Required for test data creation.");
+    }
+
     // persionally did not want to have to do this, but `/tmp` can cause some issues so...
-    let folder = format!(
-        "{}{}",
-        env::var("temp_dir").unwrap(),
-        random_name.clone().unwrap_or_default()
-    );
-
-    if !fs::exists(&folder).unwrap() {
-        fs::create_dir(&folder).unwrap_or_default();
+    // Create the temp directory if it doesn't exist
+    let folder = env::var("temp_dir").unwrap();
+    if !fs::exists(format!("{folder}")).unwrap() {
+        fs::create_dir(format!("{folder}")).unwrap_or_default();
     }
-    if !fs::exists(format!("{folder}_data")).unwrap() {
-        fs::create_dir(format!("{folder}_data")).unwrap_or_default();
+    if !fs::exists(format!("{folder}/dummy")).unwrap() {
+        fs::create_dir(format!("{folder}/dummy")).unwrap_or_default();
+    }
+    if !fs::exists(format!("{folder}/_data")).unwrap() {
+        fs::create_dir(format!("{folder}/_data")).unwrap_or_default();
     }
 
-    fs::write(
-        format!("{folder}/dummy{}.txt", random_name.unwrap_or_default()),
-        "This is a dummy file. You are a dummy for downloading this file.",
-    )
-    .expect("Failed to write dummy file");
+    // Create dummy file if it doesn't exist
+    if !fs::exists(format!("{folder}/dummy/dummy.txt")).unwrap() {
+        fs::write(
+            format!("{folder}/{DUMMY_FILE}"),
+            "This is a dummy file. You are a dummy for downloading this file.",
+        )
+        .expect("Failed to write dummy file");
+    }
 
-    path::absolute(folder).unwrap().display().to_string()
+    // Edited so that the folder path is returned as the server temporary test directory
+    // path::absolute(folder).unwrap().display().to_string()
+    env::var("server_temp_dir").unwrap()
 }
 
 pub async fn create_dummy_torrent(
     client: &Api,
-    random_name: Option<String>,
+    name: String,
 ) -> Result<TorrentCreatorTask, qbit::Error> {
-    let folder = create_test_data(random_name.clone());
+    let server_folder = create_test_data();
+    let folder = format!("{}/_data/{name}", env::var("temp_dir").unwrap());
+    fs::create_dir_all(&folder).unwrap_or_default();
+    println!(
+        "{} : {}",
+        format!("{}/{DUMMY_FILE}", env::var("temp_dir").unwrap()),
+        format!("{folder}/dummy.txt")
+    );
+    println!(
+        "{:?} : {:?}",
+        fs::exists(format!("{}/{DUMMY_FILE}", env::var("temp_dir").unwrap())),
+        fs::exists(format!("{folder}/dummy.txt"))
+    );
+    fs::copy(
+        format!("{}/{DUMMY_FILE}", env::var("temp_dir").unwrap()),
+        format!("{folder}/dummy.txt"),
+    )
+    .unwrap();
+
     let torrent = TorrentCreatorBuilder::default()
-        .source_path(&folder)
+        .source_path(format!("{server_folder}/_data/{name}"))
         .start_seeding(true)
         .private(true)
         .comment("Dummy comment for a dummy torrent")
         .source("https://example.com/dummy")
-        .torrent_file_path(format!(
-            "{folder}_data/dummy{}.torrent",
-            random_name.unwrap_or_default()
-        ))
+        .torrent_file_path(format!("{server_folder}/_data/{name}.torrent"))
         .build()
         .expect("Failed to build torrent creator");
 
